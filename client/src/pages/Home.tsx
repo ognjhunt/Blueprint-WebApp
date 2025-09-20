dd7804ed7e595f96d87b743818c57ad296b46719
Nijel Hunt <42487549+ognjhunt@users.noreply.github.com>
2025-09-20 03:17:01 +0000

    yessir

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -64,7 +64,7 @@ export default function Home() {
     }
   }, []);
 
-  //just to make changes, necessary again, yeeyee, testignasaprock
+  //just to make changes, necessary again, yeeyee, testignasaprocky
 
   useEffect(() => {
     if (currentUser) setLocation("/dashboard");
